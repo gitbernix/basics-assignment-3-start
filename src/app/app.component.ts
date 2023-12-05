35f8f7d2bdf3cb9bb2afde3c48da91445fd89f5d
gitbernix <gittaberni@gmail.com>
2023-12-05 16:24:01 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  password = 'Tuna';
+  passwordStatus = false;
+  clicks = [];
+
+  getPassword() {
+    this.passwordStatus = !this.passwordStatus;
+    this.clicks.push(this.clicks.length + 1);
+  }
+}
